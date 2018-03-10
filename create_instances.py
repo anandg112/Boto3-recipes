6f3e3d5c5411467caa76157d8acdf67b201a8a3a
anandg112 <anandg112@gmail.com>
2018-03-10 01:18:15 -0500

    creating EC2 instances

---
+++ create_instances.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import boto3
+
+ec2 = boto3.resource('ec2')
+instance = ec2.create_instances(
+  ImageId = 'ami-97785bed',
+  MinCount = 1,
+  MaxCount = 1,
+  InstanceType='t2.micro')
+print (instance[0].id)
