98645753643888302239af6f3c2d9ceb11e179b3
anandg112 <anandg112@gmail.com>
2018-04-15 01:47:46 -0400

    added script for deleting bucket

--- delete_contents.py
+++ delete_contents.py
@@ -4,6 +4,7 @@ import sys
 import boto3
 s3 = boto3.resource('s3')
 
+#will delete all the objects in each bucket passed in as an argument
 for bucket_name in sys.argv[1:]:
     bucket = s3.Bucket(bucket_name)
     for key in bucket.objects.all():
